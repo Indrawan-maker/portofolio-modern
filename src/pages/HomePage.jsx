0fe8aa5f14e77e15f595d774c195321fc29f377d
deaaprizal <deascript@icloud.com>
2025-05-11 16:48:17 +0700

    homepage full

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
 import HeroBanner from '../components/commons/molecules/HeroBanner'
+import HeroDetail from '../components/commons/molecules/HeroDetail'
 
 const HomePage = () => {
   return (
-    <><HeroBanner /></>
+    <>
+      <HeroBanner />
+      <HeroDetail />
+    </>
   )
 }
 
