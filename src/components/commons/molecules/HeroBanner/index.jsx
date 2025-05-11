acec6924ae9a3486061df70d91fe9285fe18ba85
deaaprizal <deascript@icloud.com>
2025-05-11 14:16:14 +0700

    center ball

--- src/components/commons/molecules/HeroBanner/index.jsx
+++ src/components/commons/molecules/HeroBanner/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import DoubleCurvedText from '../../atoms/DoubleCurvedText'
 
 const HeroBanner = () => {
+
+  const viewText = (type) => {
+    return <div className='absolute top-1 left-1/2 transform -translate-x-1/2 h-27 w-27 flex flex-col gap-4 items-center justify-center rounded-full'><DoubleCurvedText type={type} /></div>
+  }
+
   return (
     <section className='relative h-[460px] w-full rounded-b-4xl'>
       <img src="/images/bg-1.jpg" className='h-full w-full rounded-4xl object-cover shadow-md' />
       <div className='absolute bg-(--bg-primary-cray) h-32 w-32 rounded-full -bottom-18 -translate-x-1/2 transform left-1/2'></div>
       <div className='absolute -bottom-16 left-1/2 flex flex-col items-center justify-center gap-2 rounded-full h-28 w-28 -translate-x-1/2 transform cursor-pointer bg-(--bg-secondary-cray) shadow-xl transition-transform duration-300 ease-in-out hover:scale-105'>
-        test
+        {viewText("top")}
         <i className="fa fa-arrow-up mb-2 rotate-45 text-xl text-white"></i>
-        test
+        {viewText("bottom")}
       </div>
     </section>
   )
