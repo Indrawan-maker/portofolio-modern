a13fe2754d3d04047c0e17ac1965f642a599d094
deaaprizal <deascript@icloud.com>
2025-05-11 15:12:28 +0700

    navbar

---
+++ src/components/commons/molecules/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='absolute top-4 z-50 mt-2 h-12 w-(--my-width-nav) flex items-center justify-between bg-transparent px-4'>
+      Menu
+    </header>
+  )
+}
+
+export default Navbar
